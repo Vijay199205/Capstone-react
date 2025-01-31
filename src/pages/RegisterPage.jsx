cbde4c88b73b758f5a5d77216b8518ad771c2382
Vijay199205 <vijayamogaveera100@gmail.com>
2025-01-31 07:32:24 +0530

    Started implementing routing

---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './RegisterPage.module.css';
+
+function RegisterPage() {
+  return (
+    <div className={styles.container}>
+      Registe Page
+    </div>
+  )
+}
+
+export default RegisterPage
