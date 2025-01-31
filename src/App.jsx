0ebf2d9c2d9c251d8ac71c68f6f8a5dfd0e9dd19
Vijay199205 <vijayamogaveera100@gmail.com>
2025-01-31 07:56:03 +0530

    Not Found Page add

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
+import NotFoundPage from './pages/NotFoundPage'
 import RegisterPage from './pages/RegisterPage'
 
 function App() {
@@ -10,7 +10,13 @@ function App() {
    <BrowserRouter>
     <Routes>
       <Route path='/' element={<RegisterPage />}/>
-     
+      <Route path='/register' element={<RegisterPage />}/>
+      <Route path='/home' element={<RegisterPage/>}/>
+      <Route path='/geners' element={<RegisterPage/>}/>
+      <Route path='/carousel' element={<RegisterPage/>}/>
+      <Route path='/dashbord' element={<RegisterPage/>}/>
+      <Route path='/movie' element={<RegisterPage/>}/>
+      <Route path='*' element={<NotFoundPage/>}/>
     </Routes>
    </BrowserRouter>
   )
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import style from './NotFoundPage.module.css'
+
+function NotFoundPage() {
+  return (
+    <div className={style.conatiner}>
+      <h1>404 Page Not Found</h1>
+      <p>Sorry, the page your looking is dosenot exsit</p>
+    </div>
+  )
+}
+
+export default NotFoundPage
